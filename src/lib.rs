ff798e2330f1a5c278f59a6696a0a57e08927237
Francesco Porto <francesco.porto97@gmail.com>
2020-08-13 18:28:45 +0200

    Now using u64 as keys

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use gfa::parser::parse_gfa;
 
 /// Convert a GFA to a biedged graph if file exists
 /// otherwise return None
-fn gfa_to_biedged_graph(path : &PathBuf) -> Option<Graph::<String, String>> {
+fn gfa_to_biedged_graph(path : &PathBuf) -> Option<UnGraphMap::<u64, String>> {
     if let Some(gfa) = parse_gfa(path) {
         let graph = HashGraph::from_gfa(&gfa);
         Some(handlegraph_to_biedged_graph(&graph))
@@ -24,8 +24,8 @@ fn gfa_to_biedged_graph(path : &PathBuf) -> Option<Graph::<String, String>> {
 }
 
 /// Convert a handlegraph to a biedged graph
-fn handlegraph_to_biedged_graph(graph: &HashGraph) -> Graph::<String, String> {
-    let mut biedged : Graph::<String, String> = Graph::new();
+fn handlegraph_to_biedged_graph(graph: &HashGraph) -> UnGraphMap::<u64, String> {
+    let mut biedged : UnGraphMap::<u64, String> = UnGraphMap::new();
 
     // Create queue
     // NOTE: this is a Queue based implementation, this was done
@@ -48,28 +48,27 @@ fn handlegraph_to_biedged_graph(graph: &HashGraph) -> Graph::<String, String> {
             continue;
         }
 
+        let current_handle = Handle::pack(curr_node, false);
+        let left_id : u64 = current_handle.as_integer();
+        let right_id : u64 = current_handle.flip().as_integer();
+
         // For each node in the Handlegraph, there will be two nodes in the biedged graph
         // each representing one of the two sides
-        let id_1 = format!("{}.{}",curr_node,1);
-        let node_1 = biedged.add_node(id_1);
-        
-        let id_2 = format!("{}.{}",curr_node,2);
-        let node_2 = biedged.add_node(id_2);
+        let node_1 = biedged.add_node(left_id);
+        let node_2 = biedged.add_node(right_id);
         
         // The two nodes are connected
-        let id_edge = format!("{}",curr_node);
+        let id_edge = format!("B: {}",current_handle.unpack_number());
         biedged.add_edge(node_1, node_2, id_edge);
 
         // Look for neighbors in the Handlegraph, add edges in the biedged graph
-        let current_handle = Handle::pack(curr_node, false);
         for neighbor in handle_edges_iter(graph, current_handle, Direction::Right) {
             
             // Add first node for neighbor
-            let id_neighbor = format!("{}.{}",neighbor.id(),1);
-            let neighbor_node_biedged = biedged.add_node(id_neighbor);
+            let neighbor_node_biedged = biedged.add_node(neighbor.as_integer());
 
             // Add edge from neighbor to 
-            let id_edge = format!("{}->{}",curr_node, neighbor.id());
+            let id_edge = format!("G:{}->{}",curr_node, neighbor.id());
             biedged.add_edge(node_2, neighbor_node_biedged, id_edge);
 
             // Add to queue
@@ -85,11 +84,47 @@ fn handlegraph_to_biedged_graph(graph: &HashGraph) -> Graph::<String, String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use handlegraph::mutablehandlegraph::MutableHandleGraph;
 
     #[test]
     fn it_works() {
         let path = PathBuf::from("./input/samplePath3.gfa");
+        let graph = HashGraph::from_gfa(&parse_gfa(&path).unwrap());
         let biedged = gfa_to_biedged_graph(&path).unwrap();
         println!("{:?}", Dot::with_config(&biedged, &[Config::EdgeNoLabel]));
+        println!("{:?}", graph);
+    }
+
+    #[test]
+    fn it_works_2() {
+        let mut graph = HashGraph::new();
+        let h1 = graph.append_handle("a");
+        let h2 = graph.append_handle("c");
+        graph.create_edge(&Edge(h1,h2));
+        let biedged = handlegraph_to_biedged_graph(&graph);
+        println!("{:#?}", Dot::with_config(&biedged, &[Config::NodeNoLabel]));
+        //println!("Biedged {:#?}",Dot::new(&biedged));
+
+        println!();
+
+        println!("{:?}",graph);
+        println!("{:?}",h1.as_integer());
+        println!("{:?}",h1.flip().as_integer());
+        
+        println!();
+        //NodeId
+        println!("{:?}",h1.id());
+        //NodeId as u64
+        println!("{:?}",h1.unpack_number());
+        //NodeId does not change
+        println!("{:?}",h1.flip().unpack_number());
+
+        // println!("");
+        // println!("{:?}",h2.as_integer());
+        // println!("{:?}",h2.flip().as_integer());
+        // println!("{:?}",h2.id());
+        // println!("{:?}",h2.unpack_number());
+
+        
     }
 }
