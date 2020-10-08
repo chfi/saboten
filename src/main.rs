ee782e3f4684b41ca2c41d8140df34e31fa02a41
Christian <christian@chfi.se>
2020-10-07 20:48:48 -0500

    Better tab-delimited output

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use structopt::StructOpt;
 use rs_cactusgraph::{
     biedgedgraph::*,
     cactusgraph,
-    cactusgraph::{BiedgedWrapper, BridgeForest, CactusGraph, CactusTree},
+    cactusgraph::{BridgeForest, CactusGraph, CactusTree},
     ultrabubble::ChainPair,
 };
 
@@ -15,7 +15,7 @@ use gfa::{gfa::GFA, parser::GFAParser};
 struct Opt {
     in_gfa: PathBuf,
     /// Output JSON
-    #[structopt(short)]
+    #[structopt(short, long)]
     json: bool,
 }
 
@@ -49,23 +49,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let json = serde_json::to_string(&ultrabubble_vec)?;
         println!("{}", json);
     } else {
-        println!("x\ty\tnet\tchain/edges\tcontains");
-        println!();
-
+        println!("x\ty");
         for ((x, y), _) in ultrabubbles.iter() {
-            let net = cactus_tree.projected_node(*x);
-
-            if let Some(chain) = cactus_tree.black_edge_chain_vertex(*x) {
-                print!("{}\t{}\t{}\t\t{}", x, y, net, chain);
-            } else {
-                let x_black_edge = end_to_black_edge(*x);
-                let y_black_edge = end_to_black_edge(*y);
-
-                print!(
-                    "{}\t{}\t{}\t{:?}\t{:?}",
-                    x, y, net, x_black_edge, y_black_edge
-                );
-            }
+            println!("{}\t{}", x, y);
         }
     }
 
