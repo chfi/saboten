7395c971ceaa151c1753366508113d0affdba7af
Christian <christian@chfi.se>
2020-10-10 22:28:10 -0500

    Add ultrabubbles benchmark

--- benches/saboten_benches.rs
+++ benches/saboten_benches.rs
@@ -34,9 +34,39 @@ fn graph_transformations(c: &mut Criterion) {
     );
 }
 
+fn finding_ultrabubbles(c: &mut Criterion) {
+    let parser: GFAParser<usize, ()> = GFAParser::new();
+    let gfa: GFA<usize, ()> = parser.parse_file(&A3105_PATH).unwrap();
+
+    let orig_graph = BiedgedGraph::from_gfa(&gfa);
+
+    let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
+
+    let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
+
+    let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
+    c.bench_with_input(
+        BenchmarkId::new("finding ultrabubbles", "A-3015.gfa"),
+        &gfa,
+        |b, l| {
+            b.iter(|| {
+                let ultrabubbles = cactusgraph::find_ultrabubbles(
+                    &cactus_tree,
+                    &bridge_forest,
+                );
+            });
+        },
+    );
+}
+
 criterion_group!(
     name = transformations;
     config = Criterion::default();
     targets = graph_transformations);
 
-criterion_main!(transformations);
+criterion_group!(
+    name = ultrabubbles;
+    config = Criterion::default();
+    targets = finding_ultrabubbles);
+
+criterion_main!(transformations, ultrabubbles);
