d76fadda3d10cb71f12b69fc845b1f7dfc83e072
Christian <christian@chfi.se>
2020-10-10 22:51:24 -0500

    Bench labeling chain edges

--- benches/saboten_benches.rs
+++ benches/saboten_benches.rs
@@ -57,8 +57,31 @@ fn finding_snarls(c: &mut Criterion) {
         },
     );
 }
+
+fn labeling_chain_edges(c: &mut Criterion) {
+    let parser: GFAParser<usize, ()> = GFAParser::new();
+    let gfa: GFA<usize, ()> = parser.parse_file(&A3105_PATH).unwrap();
+
+    let orig_graph = BiedgedGraph::from_gfa(&gfa);
+
+    let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
+
+    let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
+
+    let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
+
+    let chain_pairs = cactus_tree.find_chain_pairs();
+
+    let bridge_pairs = bridge_forest.find_bridge_pairs();
+
+    c.bench_with_input(
+        BenchmarkId::new("labeling chain edges", "A-3015.gfa"),
+        &gfa,
+        |b, l| {
+            b.iter(|| {
+                cactusgraph::chain_pair_ultrabubble_labels(
                     &cactus_tree,
-                    &bridge_forest,
+                    &chain_pairs,
                 );
             });
         },
@@ -98,8 +121,13 @@ criterion_group!(
     targets = finding_snarls);
 
 criterion_group!(
+    name = labeling;
+    config = Criterion::default().sample_size(20);
+    targets = labeling_chain_edges);
+
+criterion_group!(
     name = ultrabubbles;
     config = Criterion::default().sample_size(20);
     targets = finding_ultrabubbles);
 
-criterion_main!(transformations, snarls, ultrabubbles);
+criterion_main!(transformations, snarls, labeling, ultrabubbles);
