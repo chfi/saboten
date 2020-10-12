8f0ad6fd8581c5cf5e59270bb384147b715ba622
Christian <christian@chfi.se>
2020-10-12 11:01:14 -0500

    Improved benchmarks

--- benches/saboten_benches.rs
+++ benches/saboten_benches.rs
@@ -5,127 +5,166 @@ use saboten::{
     ultrabubble::Ultrabubble,
 };
 
+use std::path::PathBuf;
+
 use gfa::{gfa::GFA, parser::GFAParser};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
-static A3105_PATH: &str = "./test/gfas/A-3105.gfa";
+static GFAPATH: &str = "./test/gfas/";
 
-fn graph_transformations(c: &mut Criterion) {
-    let parser: GFAParser<usize, ()> = GFAParser::new();
-    let gfa: GFA<usize, ()> = parser.parse_file(&A3105_PATH).unwrap();
-    c.bench_with_input(
-        BenchmarkId::new("graph transformations", "A-3015.gfa"),
-        &gfa,
-        |b, l| {
-            b.iter(|| {
-                let orig_graph = BiedgedGraph::from_gfa(&gfa);
+macro_rules! bench_graph_transforms {
+    ($name:ident, $gfa:literal) => {
+        fn $name(c: &mut Criterion) {
+            let parser: GFAParser<usize, ()> = GFAParser::new();
+            let mut path = PathBuf::from(GFAPATH);
+            path.push($gfa);
+            let gfa: GFA<usize, ()> = parser.parse_file(&path).unwrap();
+            c.bench_with_input(
+                BenchmarkId::new("graph transformations", $gfa),
+                &gfa,
+                |b, l| {
+                    b.iter(|| {
+                        let orig_graph = BiedgedGraph::from_gfa(&gfa);
 
-                let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
+                        let cactus_graph =
+                            CactusGraph::from_biedged_graph(&orig_graph);
 
-                let _cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
+                        let _cactus_tree =
+                            CactusTree::from_cactus_graph(&cactus_graph);
 
-                let _bridge_forest =
-                    BridgeForest::from_cactus_graph(&cactus_graph);
-            });
-        },
-    );
+                        let _bridge_forest =
+                            BridgeForest::from_cactus_graph(&cactus_graph);
+                    });
+                },
+            );
+        }
+    };
 }
 
-fn finding_snarls(c: &mut Criterion) {
-    let parser: GFAParser<usize, ()> = GFAParser::new();
-    let gfa: GFA<usize, ()> = parser.parse_file(&A3105_PATH).unwrap();
+macro_rules! bench_finding_snarls {
+    ($name:ident, $gfa:literal) => {
+        fn $name(c: &mut Criterion) {
+            let parser: GFAParser<usize, ()> = GFAParser::new();
+            let mut path = PathBuf::from(GFAPATH);
+            path.push($gfa);
+            let gfa: GFA<usize, ()> = parser.parse_file(&path).unwrap();
 
-    let orig_graph = BiedgedGraph::from_gfa(&gfa);
+            let orig_graph = BiedgedGraph::from_gfa(&gfa);
 
-    let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
+            let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
 
-    let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
+            let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
 
-    let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
-    c.bench_with_input(
-        BenchmarkId::new("finding snarls", "A-3015.gfa"),
-        &gfa,
-        |b, l| {
-            b.iter(|| {
-                cactus_tree.find_chain_pairs();
+            let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
+            c.bench_with_input(
+                BenchmarkId::new("finding snarls", $gfa),
+                &gfa,
+                |b, l| {
+                    b.iter(|| {
+                        cactus_tree.find_chain_pairs();
 
-                bridge_forest.find_bridge_pairs();
-            });
-        },
-    );
+                        bridge_forest.find_bridge_pairs();
+                    });
+                },
+            );
+        }
+    };
 }
 
-fn labeling_chain_edges(c: &mut Criterion) {
-    let parser: GFAParser<usize, ()> = GFAParser::new();
-    let gfa: GFA<usize, ()> = parser.parse_file(&A3105_PATH).unwrap();
+macro_rules! bench_label_chain_edges {
+    ($name:ident, $gfa:literal) => {
+        fn $name(c: &mut Criterion) {
+            let parser: GFAParser<usize, ()> = GFAParser::new();
+            let mut path = PathBuf::from(GFAPATH);
+            path.push($gfa);
+            let gfa: GFA<usize, ()> = parser.parse_file(&path).unwrap();
 
-    let orig_graph = BiedgedGraph::from_gfa(&gfa);
+            let orig_graph = BiedgedGraph::from_gfa(&gfa);
+            let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
+            let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
+            let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
+            let chain_pairs = cactus_tree.find_chain_pairs();
+            let bridge_pairs = bridge_forest.find_bridge_pairs();
 
-    let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
-
-    let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
-
-    let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
-
-    let chain_pairs = cactus_tree.find_chain_pairs();
-
-    let bridge_pairs = bridge_forest.find_bridge_pairs();
-
-    c.bench_with_input(
-        BenchmarkId::new("labeling chain edges", "A-3015.gfa"),
-        &gfa,
-        |b, l| {
-            b.iter(|| {
-                cactusgraph::chain_pair_ultrabubble_labels(
-                    &cactus_tree,
-                    &chain_pairs,
-                );
-            });
-        },
-    );
+            c.bench_with_input(
+                BenchmarkId::new("labeling chain edges", $gfa),
+                &gfa,
+                |b, l| {
+                    b.iter(|| {
+                        cactusgraph::chain_pair_ultrabubble_labels(
+                            &cactus_tree,
+                            &chain_pairs,
+                        );
+                    });
+                },
+            );
+        }
+    };
 }
 
-fn finding_ultrabubbles(c: &mut Criterion) {
-    let parser: GFAParser<usize, ()> = GFAParser::new();
-    let gfa: GFA<usize, ()> = parser.parse_file(&A3105_PATH).unwrap();
+macro_rules! bench_finding_ultrabubbles {
+    ($name:ident, $gfa:literal) => {
+        fn $name(c: &mut Criterion) {
+            let parser: GFAParser<usize, ()> = GFAParser::new();
+            let mut path = PathBuf::from(GFAPATH);
+            path.push($gfa);
+            let gfa: GFA<usize, ()> = parser.parse_file(&path).unwrap();
 
-    let orig_graph = BiedgedGraph::from_gfa(&gfa);
+            let orig_graph = BiedgedGraph::from_gfa(&gfa);
+            let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
+            let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
+            let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
 
-    let cactus_graph = CactusGraph::from_biedged_graph(&orig_graph);
-
-    let cactus_tree = CactusTree::from_cactus_graph(&cactus_graph);
-
-    let bridge_forest = BridgeForest::from_cactus_graph(&cactus_graph);
-    c.bench_with_input(
-        BenchmarkId::new("finding ultrabubbles", "A-3015.gfa"),
-        &gfa,
-        |b, l| {
-            b.iter(|| {
-                cactusgraph::find_ultrabubbles(&cactus_tree, &bridge_forest);
-            });
-        },
-    );
+            c.bench_with_input(
+                BenchmarkId::new("finding ultrabubbles", $gfa),
+                &gfa,
+                |b, l| {
+                    b.iter(|| {
+                        cactusgraph::find_ultrabubbles(
+                            &cactus_tree,
+                            &bridge_forest,
+                        );
+                    });
+                },
+            );
+        }
+    };
 }
+
+bench_graph_transforms!(transform_a3015, "A-3105.gfa");
+bench_graph_transforms!(transform_covid, "relabeledSeqs.nopaths.gfa");
+
+bench_finding_snarls!(find_snarls_a3015, "A-3105.gfa");
+bench_finding_snarls!(find_snarls_covid, "relabeledSeqs.nopaths.gfa");
+
+bench_label_chain_edges!(label_chain_edges_a3015, "A-3105.gfa");
+bench_label_chain_edges!(label_chain_edges_covid, "relabeledSeqs.nopaths.gfa");
+
+bench_finding_ultrabubbles!(find_ultrabubbles_a3015, "A-3105.gfa");
+bench_finding_ultrabubbles!(
+    find_ultrabubbles_covid,
+    "relabeledSeqs.nopaths.gfa"
+);
 
 criterion_group!(
     name = transformations;
     config = Criterion::default();
-    targets = graph_transformations);
+    targets = transform_a3015, transform_covid);
 
 criterion_group!(
     name = snarls;
     config = Criterion::default();
-    targets = finding_snarls);
+    targets = find_snarls_a3015, find_snarls_covid);
 
 criterion_group!(
     name = labeling;
     config = Criterion::default().sample_size(20);
-    targets = labeling_chain_edges);
+    targets = label_chain_edges_a3015,label_chain_edges_covid);
 
 criterion_group!(
     name = ultrabubbles;
     config = Criterion::default().sample_size(20);
-    targets = finding_ultrabubbles);
+    targets = find_ultrabubbles_a3015, find_ultrabubbles_covid);
 
 criterion_main!(transformations, snarls, labeling, ultrabubbles);
