6a0c04386c25b88677eb8a7217a8309c703b8ca0
Christian Fischer <christian@chfi.se>
2021-08-20 14:51:16 -0500

    basic, more robust node projection map

--- src/projection_graph.rs
+++ src/projection_graph.rs
@@ -1,15 +1,60 @@
-use handlegraph::{disjoint::*, packedgraph::PackedGraph};
+use handlegraph::{disjoint::*, handle::Handle, packedgraph::PackedGraph};
 
 use std::sync::Arc;
 
-#[derive(Clone)]
 pub struct ProjectedPackedGraph {
     graph: Arc<PackedGraph>,
-    // projection: Projection
+    projection: Projection,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BNode(u64);
+
+impl From<Handle> for BNode {
+    fn from(h: Handle) -> Self {
+        let id = h.id().0;
+        let ix = (id - 1) / 2;
+        if h.is_reverse() {
+            BNode(ix)
+        } else {
+            BNode(ix + 1)
+        }
+    }
 }
 
 pub struct Projection {
     pub size: usize,
     disj: DisjointSets,
-    inverse: Vec<u64>,
+}
+
+impl Projection {
+    pub fn new(node_count: usize) -> Self {
+        let size = node_count;
+
+        let disj = DisjointSets::new(size);
+
+        Self { size, disj }
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    pub fn find_handle(&self, handle: Handle) -> BNode {
+        let bn = BNode::from(handle);
+        let projected = self.disj.find(bn.0);
+        BNode(projected)
+    }
+
+    pub fn find_biedged_node(&self, node: BNode) -> BNode {
+        BNode(self.disj.find(node.0))
+    }
+
+    pub fn same(&self, x: BNode, y: BNode) -> bool {
+        self.disj.same(x.0, y.0)
+    }
+
+    pub fn unite(&self, x: BNode, y: BNode) -> BNode {
+        BNode(self.disj.unite(x.0, y.0))
+    }
 }
